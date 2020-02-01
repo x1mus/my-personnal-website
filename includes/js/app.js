e51a57dc134257ca590d40257c173a0b3471f274
Laenen Maximilien <laenen.maximilien@outlook.com>
2020-02-01 17:50:06 +0100

    Includes loading of the different page within the main;

--- includes/js/app.js
+++ includes/js/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    $(".main_content").load("home.html");
+	// Loading of the default page of the site.
+	$("#main_content").load("home.html");
+
+	// For each link in the navigation bar when clicked
+	$('.navbar > a').on('click', function () {
+		// We add the class .active to the link that is clicked and remove it from the others
+    	$(this).addClass('active').siblings().removeClass('active');
+
+    	// Then we load the correct page with help of the id of that link
+    	$('#main_content').load($(this).attr('id') + '.html');
+    });
 });
