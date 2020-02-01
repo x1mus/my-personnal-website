f134c633eb8d4677edc567069f2da3fb11b1beb3
Laenen Maximilien <laenen.maximilien@outlook.com>
2020-02-01 19:41:22 +0100

    NavBar responsive;

--- includes/js/app.js
+++ includes/js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	// Loading of the default page of the site.
-	$("#main_content").load("home.html");
+	$('#main_content').load('home.html');
 
 	// For each link in the navigation bar when clicked
-	$('.navbar > a').on('click', function () {
+	$('.navbar > div > a').on('click', function () {
 		// We add the class .active to the link that is clicked and remove it from the others
-    	$(this).addClass('active').siblings().removeClass('active');
+    	$(this).addClass('active').parent('div').siblings('div').find('a').removeClass('active');
 
     	// Then we load the correct page with help of the id of that link
     	$('#main_content').load($(this).attr('id') + '.html');
