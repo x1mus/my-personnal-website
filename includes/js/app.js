07a35845fb064b34c94c230180d83b5f8337b186
Laenen Maximilien <laenen.maximilien@outlook.com>
2020-02-24 20:36:53 +0100

    "Initial commit"; Totally new design; Restart from scratch;

--- includes/js/app.js
+++ includes/js/app.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 	// Loading of the default page of the site.
-	$('#main_content').load('home.html');
+	$('#main').load('home.html');
 
 	// For each link in the navigation bar when clicked
-	$('.navbar > div > a').on('click', function () {
+	$('.navbar > div').on('click', function () {
 		// We add the class .active to the link that is clicked and remove it from the others
-    	$(this).addClass('active').parent('div').siblings('div').find('a').removeClass('active');
+    	$(this).children('a').addClass('active')
+    	$(this).siblings('div').find('a').removeClass('active');
 
     	// Then we load the correct page with help of the id of that link
-    	$('#main_content').load($(this).attr('id') + '.html');
+    	$('#main').load($(this).children('a').attr('id') + '.html');
     });
 });
